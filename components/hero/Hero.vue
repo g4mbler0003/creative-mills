7a8b0fa0cb333c048264a6f70b66ffe6648fb00e
g4mbler0003 <g4mbler0003@gmail.com>
2023-01-25 21:04:21 -0500

    fixed bg color

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bghero w-6/8 p-5 lg:p-16">
-    <div class="bg-[#FAC27B] px-5 w-2/5">
+    <div class="bgherocolor px-5 w-2/5">
     <h1 class="text-brown px-5 text-6xl">
       Creative Mills
     </h1>
@@ -13,5 +13,8 @@
 <style lang="scss" scoped>
   .bghero {
     background: url('../../static/logo3.jpeg');
+  }
+  .bgherocolor {
+    background-color: #FAC27B;
   }
   </style>
