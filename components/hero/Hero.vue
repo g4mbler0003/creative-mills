95a72ff49b5769df35d42010a4aac567cf73d71d
g4mbler0003 <g4mbler0003@gmail.com>
2023-01-25 20:54:53 -0500

    fixed bg link

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[url('/_nuxt/img/logo3.3783b7a.jpeg')] w-6/8 p-5 lg:p-16">
+  <div class="bghero w-6/8 p-5 lg:p-16">
     <div class="bg-[#FAC27B] px-5 w-2/5">
     <h1 class="text-brown px-5 text-6xl">
       Creative Mills
@@ -10,3 +10,8 @@
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+  .bghero {
+    background: url('../../static/logo3.jpeg');
+  }
+  </style>
